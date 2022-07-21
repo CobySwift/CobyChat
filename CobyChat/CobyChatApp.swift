364301833c76262fd3d9baba0d234d45f9e821ef
kimdoyoung <57849386+coby5502@users.noreply.github.com>
2022-07-21 22:59:28 +0900

    Initial Commit

---
+++ CobyChat/CobyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobyChatApp.swift
+//  CobyChat
+//
+//  Created by COBY_PRO on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CobyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
