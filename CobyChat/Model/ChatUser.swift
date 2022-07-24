6f526fff5afb8cb7ddc2199c594d21a521beb63d
Doyoung <coby5502@gmail.com>
2022-07-24 13:06:07 +0900

    Fetch Current User and Load Image SDWebImage

---
+++ CobyChat/Model/ChatUser.swift
@@ -0,0 +1,12 @@
+//
+//  ChatUser.swift
+//  CobyChat
+//
+//  Created by COBY_PRO on 2022/07/24.
+//
+
+import FirebaseFirestore
+
+struct ChatUser {
+    let uid, email, profileImageUrl: String
+}
